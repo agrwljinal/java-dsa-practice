12fe1a20d7878dcf78f5f6da774f20b2a8cc43c8
agrwljinal <269049701+agrwljinal@users.noreply.github.com>
2026-05-25 04:47:55 +0530

    Add prime factorization

---
+++ Factorization/PrimeFactorization.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+import javax.sound.midi.SysexMessage;
+
+public class PrimeFactorization {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        for (int div = 2; div*div<=n; div++) {
+
+            while (n%div==0) {
+                System.out.println(div);
+                n = n/div;
+            }
+        }
+
+        // if there is something greater than 1 left as div then that entire will be a prime factor of input number because there was no sub factor found till its root.
+        if(n!=1) {
+            System.out.print(n);
+        }
+
+        sc.close();
+    }
+}
