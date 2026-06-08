eda34d6ff8141ef931ac825c38683cd4b4768371
agrwljinal <269049701+agrwljinal@users.noreply.github.com>
2026-06-08 06:32:00 +0530

    Add bar chart solution

---
+++ Arrays/BarChart/BarChart.java
@@ -0,0 +1,40 @@
+import java.util.*;
+
+public class BarChart {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int[] a = new int[n];
+
+        for (int i = 0; i<n; i++) {
+            a[i] = sc.nextInt();
+        }
+
+        int max = a[0];
+
+        for (int i = 0; i<n; i++) {
+            if (a[i]>max) {
+                max = a[i];
+            }
+        }
+
+        for (int i = 0; i < max; i++) {
+            for (int j = 0; j < n; j++) {
+
+                if (max - i <= a[j]) {
+                    System.out.print("*\t");
+                }
+
+                else {
+                    System.out.print("\t");
+                }
+            }
+
+            System.out.println();
+        }
+
+        sc.close();
+    }
+}
---
+++ Arrays/BarChart/README.md
@@ -0,0 +1,21 @@
+# Bar Chart
+
+---
+
+## Problem Statement
+
+Given an array of `n` integers. Print a bar chart of asterisks representing the array.
+
+--- 
+
+### Example
+
+Input: <br>
+3<br>2<br>1<br>3
+<br><br>
+Output: <br>
+```
+        *
+*       *
+*   *   *
+```
